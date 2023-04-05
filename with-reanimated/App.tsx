a295836e29511e21ec397aa2d61d3629b1295ce4
Cavit Baturalp Gürdin <cavit.gurdin@gmail.com>
2023-04-05 08:10:44 +0200

    refactor

--- with-reanimated/App.tsx
+++ with-reanimated/App.tsx
@@ -1,11 +1,11 @@
+import React from "react";
+import { View, Button, Platform, StyleSheet } from "react-native";
 import Animated, {
   useSharedValue,
   withTiming,
   useAnimatedStyle,
   Easing,
 } from "react-native-reanimated";
-import { View, Button, Platform } from "react-native";
-import React from "react";
 
 if (Platform.OS === "web") {
   // more info: https://github.com/software-mansion/react-native-reanimated/issues/3355
@@ -27,27 +27,29 @@ export default function App() {
     };
   });
 
+  const onPressButton = () => {
+    randomWidth.value = Math.random() * 350;
+  };
+
   return (
-    <View
-      style={{
-        flex: 1,
-        alignItems: "center",
-        justifyContent: "center",
-        flexDirection: "column",
-      }}
-    >
-      <Animated.View
-        style={[
-          { width: 100, height: 80, backgroundColor: "black", margin: 30 },
-          style,
-        ]}
-      />
-      <Button
-        title="toggle"
-        onPress={() => {
-          randomWidth.value = Math.random() * 350;
-        }}
-      />
+    <View style={styles.container}>
+      <Animated.View style={[styles.animatedViewContainer, style]} />
+      <Button title="Hello" onPress={onPressButton} />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    flexDirection: "column",
+  },
+  animatedViewContainer: {
+    width: 100,
+    height: 80,
+    backgroundColor: "black",
+    margin: 30,
+  },
+});
